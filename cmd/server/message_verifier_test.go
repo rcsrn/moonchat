66624ce1d6a78a4e155f3b35251a59a7437a749e
nsgbanks@ciencias.unam.mx <nsgbanks@ciencias.unam.mx>
2022-10-20 00:30:32 -0500

    starting tests of message_verifier.go

--- cmd/server/message_verifier_test.go
+++ cmd/server/message_verifier_test.go
@@ -1,1 +1,14 @@
 package main
+
+import (
+	"testing"
+	"github.com/rcsrn/moonchat/cmd/message"
+)
+
+func toMap(message message.Message) (map[string]string) {
+	return nil
+}
+
+func testVerifyIdentifyMessage(t *testing.T) {
+	
+}
