2064f03591176927b0fe32f51e91108941580f43
rodrigo <nsgbanks@ciencias.unam.mx>
2022-10-21 20:32:36 -0500

    skeletons of client_processor and message_verifier.go added

--- internal/client/client_processor.go
+++ internal/client/client_processor.go
@@ -2,17 +2,19 @@ package client
 
 import (
 	"net"
+	"errors"
 )
 
 type ClientProcessor struct {
 	connection net.Conn
 	identified bool
 	username string
-	messageCreator *messageCreator
+	creator *messageCreator
+	verifier *messageVerifier
 }
 
 func getClientInstance(connection net.Conn) *ClientProcessor {
-	return &ClientProcessor{connection, false, "", &messageCreator{}}
+	return &ClientProcessor{connection, false, "", &messageCreator{}, &messageVerifier{}}
 }
 
 func (processor *ClientProcessor) setConnection(connection net.Conn) {
@@ -23,15 +25,72 @@ func (processor *ClientProcessor) sendMessage(message []byte) {
 	processor.connection.Write(message)
 }
 
-func (processor *ClientProcessor) ProcessMessage(message []string) {
+func (processor *ClientProcessor) ProcessMessage(message []string) error {
+	if !processor.verifier.isValid(message) {
+		return createError("invalid")
+	}
 	firstWord := message[0]
 	switch firstWord {
-	case CLOSE: closeCase(processor)
+	case CLOSE: processor.disconnect()
+		return nil
+	case LEAVE_ROOM: processor.leaveRoom()
+		return nil
+	case STATUS: processor.changeStatus()
+		return nil
+	case USER_LIST: processor.requestUserList()
+		return nil
+	case PRIVATE: processor.sendPrivateMessage()
+		return nil
+	case NEW_ROOM: processor.createNewRoom()
+		return nil
+	case INVITE: processor.inviteUsersToRoom()
+		return nil
+	case ROOM_MESSAGE: processor.sendRoomMessage()
+		return nil
+	default: processor.sendPublicMessage()
+		return nil
 	}
 }
 
-func closeCase(processor *ClientProcessor) {
-	disconnectMessage := processor.messageCreator.getDisconnectMessage()
+func (processor *ClientProcessor) disconnect() {
+	disconnectMessage := processor.creator.getDisconnectMessage()
 	processor.sendMessage(disconnectMessage)
 }
 
+func (processor *ClientProcessor) leaveRoom() {
+	leaveRoomMessage := processor.creator.getLeaveRoomMessage()
+	processor.sendMessage(leaveRoomMessage)
+}
+ 
+func (processor *ClientProcessor) changeStatus() {
+	
+}
+
+func (processor *ClientProcessor) requestUserList() {
+	
+}
+
+func (processor *ClientProcessor) sendPrivateMessage() {
+	
+}
+
+func (processor *ClientProcessor) createNewRoom() {
+	
+}
+
+func (processor *ClientProcessor) inviteUsersToRoom() {
+	
+}
+
+func (processor *ClientProcessor) sendRoomMessage() {
+	
+}
+
+func (processor *ClientProcessor) sendPublicMessage() {
+	
+}
+
+
+func createError(errorMessage string) error {
+	return errors.New(errorMessage)
+}
--- internal/client/constants.go
+++ internal/client/constants.go
@@ -2,4 +2,11 @@ package client
 
 const (
 	CLOSE = "/close"
+	LEAVE_ROOM = "/leaveroom"
+	STATUS = "/status"
+	USER_LIST = "userlist"
+	PRIVATE = "/private"
+	NEW_ROOM = "/newroom"
+	INVITE = "invite"
+	ROOM_MESSAGE = "/roommessage"
 )
